96cca7670f42f542b0b93db82d18bafb959949ec
Raymond Jette <rjette@mestek.com>
2022-03-20 12:50:51 -0400

    Eliminate duplicate code in Get-DiskUsageByDirectory.

--- Files and Folders/Get-DiskUsageByDirectory.ps1
+++ Files and Folders/Get-DiskUsageByDirectory.ps1
@@ -21,11 +21,11 @@ Function Get-DiskUsageByDirectory
         The unit size will be reported as.  Accepted values are TB, GB, MB, and KB.
 
         .EXAMPLE
-        Get-DiskUsage
+        Get-DiskUsage -Path C:\
         For each directory in path this will output the size of the directory including it's subdirectories.
 
         .EXAMPLE
-        Get-DiskUsage -Recurse
+        Get-DiskUsage -Path C:\ -Recurse
         For each directory in path this will output the size of the directory and the subdirectories.  The size
         of the subdirectory is not included in the size of the directory as the subdirectory is also output.
     #>
@@ -51,35 +51,38 @@ Function Get-DiskUsageByDirectory
         [math]::round($Size / "1$Unit", $PlacesAfterDecimal)
     }
 
+    # Setup parameter for Get-ChildItem
+    $GetChildItemSplat = @{
+        Directory = $true
+        Path      = $Path
+    }
+    $GetChildItemSizeSplat = @{
+        ErrorAction = 'SilentlyContinue'
+    }
 
+    # If the recurse parameter was specified we add recurse to the Get-ChildItem to get the directories
+    # in path.  If the recurse parameter was not specified we add recurse to the Get-ChildItem used to
+    # get the disk space so we can account for the space used by subdirectories.
     if ($PSBoundParameters.ContainsKey('Recurse'))
     {
-        Get-ChildItem -Directory -Path $Path -Recurse | ForEach-Object {
-            Write-Progress -Activity "Getting Size for $($PSItem.FullName)"
-            $Size = ($PSItem | Get-ChildItem -ErrorAction 'SilentlyContinue' | Measure-Object -Sum Length -ErrorAction 'SilentlyContinue').Sum
-            if ($PSBoundParameters.ContainsKey('Unit'))
-            {
-                $Size = Format-Size -Size $Size -Unit $Unit -PlacesAfterDecimal 2
-            }
-            [PSCustomObject]@{
-                Directory = $PSItem.FullName
-                "Size$Unit" = $Size
-            }
-        }
+        $GetChildItemSplat.add('Recurse', $true)
     }
     else
     {
-        Get-ChildItem -Directory -Path $Path | ForEach-Object {
-            Write-Progress -Activity "Getting Size for $($PSItem.FullName)"
-            $Size = ($PSItem | Get-ChildItem -Recurse -ErrorAction 'SilentlyContinue' | Measure-Object -Sum Length -ErrorAction 'SilentlyContinue').Sum
-            if ($PSBoundParameters.ContainsKey('Unit'))
-            {
-                $Size = Format-Size -Size $Size -Unit $Unit -PlacesAfterDecimal 2         
-            }
-            [PSCustomObject]@{
-                Directory = $PSItem.FullName
-                "Size$Unit" = $Size
-            }
+        $GetChildItemSizeSplat.add('Recurse', $true)
+    }
+
+    # Get size of directories in path.
+    Get-ChildItem @GetChildItemSplat | ForEach-Object {
+        Write-Progress -Activity "Getting Size for $($PSItem.FullName)"
+        $Size = ($PSItem | Get-ChildItem @GetChildItemSizeSplat | Measure-Object -Sum Length -ErrorAction 'SilentlyContinue').Sum
+        if ($PSBoundParameters.ContainsKey('Unit'))
+        {
+            $Size = Format-Size -Size $Size -Unit $Unit -PlacesAfterDecimal 2
+        }
+        [PSCustomObject]@{
+            Directory = $PSItem.FullName
+            "Size$Unit" = $Size
         }
     }
 }
